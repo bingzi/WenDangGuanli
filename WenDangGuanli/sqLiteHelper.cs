bf6271f4a128cb515c806d300966e7250a040dc2
meng <dz6168@163.com>
2018-10-15 21:59:32 +0800

    添加项目文件。

---
+++ WenDangGuanli/sqLiteHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+
+
+namespace WenDangGuanli
+{
+  public class sqLiteHelper
+    {
+        SQLiteConnection sQLiteConnection;
+
+        public sqLiteHelper(SQLiteConnection sQLiteConnection)
+        {
+            this.sQLiteConnection = sQLiteConnection;
+        }
+    }
+}
