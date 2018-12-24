88286e35417b0e8deecf2857011952915231841a
meng <dz6168@163.com>
2018-12-24 23:05:25 +0800

    sd

--- WenDangGuanli/PeiFang.cs
+++ WenDangGuanli/PeiFang.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class Class1
 {
@@ -26,45 +27,47 @@ public class Class1
     public string name { get; set; }
 }
 
-public class Root
-{
-    /// <summary>
-    /// 
-    /// </summary>
-    public string id { get; set; }
-    /// <summary>
-    /// 龙虾大餐
-    /// </summary>
-    public string name { get; set; }
-    /// <summary>
-    /// 
-    /// </summary>
-    public string img { get; set; }
-    /// <summary>
-    /// 
-    /// </summary>
-    public string type { get; set; }
-    /// <summary>
-    /// 【配方】龙虾
-
-
-    /// </summary>
-    public string jianjie { get; set; }
-    /// <summary>
-    /// 
-    /// </summary>
-
-    /// 
-    /// </summary>
-    public string listorder { get; set; }
-    /// <summary>
-    /// 
-    /// </summary>
-    public string zjtj { get; set; }
-    /// <summary>
-    /// 
-    /// </summary>
-
-    public Need_sp_info need_sp_info { get; set; }
-}
+    public class Root
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string id { get; set; }
+        /// <summary>
+        /// 龙虾大餐
+        /// </summary>
+        public string name { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string img { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string type { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string jianjie { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<HcItem> hc { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string listorder { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string zjtj { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<string> hc_sp_id { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Need_sp_info need_sp_info { get; set; }
+    }
 }
